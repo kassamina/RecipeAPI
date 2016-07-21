e9ab164e47658e7a81edaa9d517ee25f12b63cd3
kassamina <venna138@gmail.com>
2016-07-21 11:50:38 -0400

    ingredient struct, and alterRecipe

--- RecipeAPI.go
+++ RecipeAPI.go
@@ -1,12 +1,17 @@
 package main
 /*
 todo
-	-port is fixed, parameter?
-	-document! comment!
+	-port is fixed, should be parameter?
 	-ingredients as struct
-	-more calls to updates ingredients
-	-more testing
+		-more calls to updates ingredients
 	-the declaration of Recipe should be in it's own file. 
+
+	improvements
+		-catagories, breakfast, etc.
+			implemented as in relational DB
+			breakfast would be a table with pointers to related recipes, ie. pointer to fried egg, poached egg, etc. 
+
+
 */
 import (
     "fmt"
@@ -16,11 +21,17 @@ import (
     "strconv"
 )
 
+type Ingredient struct {
+	Quantity 	float32  `json:"quantity`
+	Unit		string `json:"unit"`
+    Name 		string `json:"name"`
+}
+
 type Recipe struct {
-	ID int `json:"id`
-    Name string `json:"name"`
-    Ingredients string `json:"ingredients"`
-    Instructions string `json:"instructions"`
+	ID 				int `json:"id`
+    Name 			string `json:"name"`
+    Ingredients 	[]Ingredient
+    Instructions 	string `json:"instructions"`
 }
 
 
@@ -31,6 +42,9 @@ func homePage(w http.ResponseWriter, r *http.Request){
     fmt.Fprintf(w, "Hello World!")
     fmt.Println("homepage hit?")
 }
+
+
+
 
 /*
 	Implemented because indicies in recipesDB are garunteed to be in sorted order,
@@ -56,14 +70,15 @@ func binarySearch(low int, high int, target int) int {
 
 
 
+
 /*	returnRecipe
-	Input: 	Expects a http POST with form value ID, containing the recipeID of the recipe being requested.
-			http://localhost:8081/return
+	Input: 	Expects a http post body parameter, named "ID" containing the recipeID you wish to be returned.
+			request should be sent to http://localhost:8081/return
 			
 	Return:	json encoded Recipe
 	
-	Error:	
-			should likely be implemented as a GET */
+	Error:	Returns error message in body of response
+			should likely be implemented as a GET, with a url parameter */
 func returnRecipe(w http.ResponseWriter, r *http.Request) {
 	ID,err := strconv.Atoi(r.FormValue("ID"))
 	if err != nil {
@@ -88,6 +103,45 @@ func returnRecipe(w http.ResponseWriter, r *http.Request) {
 }//returnRecipe
 
 
+
+
+/*	delRecipe
+	Input: 	Expects a http post body parameter, named "ID" containing the recipeID you wish to delete
+			request should be sent to http://localhost:8081/delete
+	Return:	Sends back the deleted recipe's ID on success
+
+	Error: 	Returns error message in body of response*/
+func delRecipe(w http.ResponseWriter, r *http.Request){
+	ID,err := strconv.Atoi(r.FormValue("ID"))
+	if err != nil {
+		panic(err)
+	}
+
+    fmt.Println("recieved request to delete recipeID: %d", ID)
+
+    i := -1
+    if ID < nextID && ID >= 0 {
+    	i = binarySearch(0, nextID-1, ID)
+    	if i == -1 {
+    		fmt.Println("I'm sorry, that's no longer a valid recipeID.")
+    		fmt.Fprintf(w,"I'm sorry, that's no longer a valid recipeID.")
+    	} else {
+    		//appends the list of all recipes with id's <i, to the list of all recipes with id's >i
+			recipeDB = append(recipeDB[:i], recipeDB[i+1:]...)
+			fmt.Fprintf(w, "%d", i)
+			fmt.Println("Successfully deleted recipe with id == %d", i)
+    	}
+    } else {
+    	fmt.Println("Invalid ID! deletion request denied!")
+    	fmt.Fprintf(w,"Invalid ID! deletion request denied!")
+    }
+
+    fmt.Println("Successfully deleted recipe with id == %d", i)
+}//delRecipe
+
+
+
+
 /*	returnAllRecipes
 	Input:	Any request sent to http://localhost:8081/all
 
@@ -98,6 +152,8 @@ func returnAllRecipes(w http.ResponseWriter, r *http.Request){
     fmt.Println("Recieved request to return all recipes")
     json.NewEncoder(w).Encode(recipeDB)
 }//returnAllRecipes
+
+
 
 
 /*	addRecipe
@@ -123,21 +179,26 @@ func addRecipe(w http.ResponseWriter, r *http.Request){
 }//addRecipe
 
 
-/*	delRecipe
-	Input: http post body parameter, named "ID" containing the recipeID you wish to delete
-
-	Return:	Sends back the deleted recipe's ID on success
-
-	Error: 	Returns error message in body of response*/
-func delRecipe(w http.ResponseWriter, r *http.Request){
-	ID,err := strconv.Atoi(r.FormValue("ID"))
+/*	alterRecipe
+	Input: 	Expected json formated Recipe in body of request sent to http://localhost:8081/alter
+			the recipe's id should match an existing recipe
+			
+	Return:	The updated json encoded Recipe in body of response
+	
+	Error:	Returns error message in body of response
+			should likely be implemented as a GET, with a url parameter */
+func alterRecipe(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var alteredRecipe Recipe
+	err := decoder.Decode(&alteredRecipe)
 	if err != nil {
+		fmt.Fprintf(w, "Error: Inproperly formated Recipe")
 		panic(err)
 	}
 
-    fmt.Println("recieved request to delete recipeID: %d", ID)
+	ID := alteredRecipe.ID
 
-    i := -1
+	i := -1
     if ID < nextID && ID >= 0 {
     	i = binarySearch(0, nextID-1, ID)
     	if i == -1 {
@@ -145,17 +206,16 @@ func delRecipe(w http.ResponseWriter, r *http.Request){
     		fmt.Fprintf(w,"I'm sorry, that's no longer a valid recipeID.")
     	} else {
     		//appends the list of all recipes with id's <i, to the list of all recipes with id's >i
-			recipeDB = append(recipeDB[:i], recipeDB[i+1:]...)
-			fmt.Fprintf(w, "%d", i)
+			recipeDB[i] = alteredRecipe
+			json.NewEncoder(w).Encode(recipeDB[i])
+			fmt.Println("Successfully altered recipe with id == %d", i)
     	}
     } else {
-    	fmt.Println("Invalid ID! deletion request denied!")
-    	fmt.Fprintf(w,"Invalid ID! deletion request denied!")
+    	fmt.Println("Invalid ID! alteration request denied!")
+    	fmt.Fprintf(w,"Invalid ID! alteration request denied!")
     }
+}//alterRecipe
 
-    fmt.Fprintf(w, "deletes a specific recipe")
-    fmt.Println("Endpoint Hit: delRecipe")
-}//delRecipe
 
 func handleRequests() {
     http.HandleFunc("/", homePage)
@@ -163,14 +223,15 @@ func handleRequests() {
     http.HandleFunc("/return", returnRecipe)
     http.HandleFunc("/delete", delRecipe)
     http.HandleFunc("/add", addRecipe)
+    http.HandleFunc("/alter", alterRecipe)
     log.Fatal(http.ListenAndServe(":8081", nil))
 }//handRequests
 
 func main() {
 	//create the slice which will store all of the recipes; 
 	recipeDB = make([]Recipe, 0)
-	recipeDB = append(recipeDB, Recipe{ID: 0, Name: "Fried Egg", Ingredients: "1 Egg", Instructions: "Fry the egg."})
-	recipeDB = append(recipeDB, Recipe{ID: 1, Name: "Poached Egg", Ingredients: "1 Egg", Instructions: "Poach the egg."})
+	recipeDB = append(recipeDB, Recipe{ID: 0, Name: "Fried Egg", Ingredients: []Ingredient{{1.0,"", "egg"}, {2.0,"teaspoons", "butter"}}, Instructions: "Fry the egg(s) in the butter."})
+	recipeDB = append(recipeDB, Recipe{ID: 1, Name: "Poached Egg", Ingredients: []Ingredient{{1.0,"", "egg"}}, Instructions: "Poach the egg(s)."})
 	nextID = 2
 
 	//fmt.Println(recipeDB[0])
--- RecipeTest.go
+++ RecipeTest.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-    "fmt"
+    //"fmt"
     "bytes"
     "net/http"
     "encoding/json"
@@ -10,15 +10,21 @@ import (
     "strconv"
     "net/url"
 )
+type Ingredient struct {
+    Quantity    float32  `json:"quantity`
+    Unit        string `json:"unit"`
+    Name        string `json:"name"`
+}
+
 type Recipe struct {
-    ID int `json:"id`
-    Name string `json:"name"`
-    Ingredients string `json:"ingredients"`
-    Instructions string `json:"instructions"`
+    ID              int `json:"id`
+    Name            string `json:"name"`
+    Ingredients     []Ingredient
+    Instructions    string `json:"instructions"`
 }
 
 func testAddRecipe() {
-    newRecipe := Recipe{ID: 0, Name: "Boiled Egg", Ingredients: "1 Egg", Instructions: "Boil the egg."}
+    newRecipe := Recipe{ID: 0, Name: "Boiled Egg", Ingredients: []Ingredient{{1.0,"", "egg"}}, Instructions: "Boil the egg(s)."}
    
     b := new(bytes.Buffer)
     json.NewEncoder(b).Encode(newRecipe)
@@ -31,20 +37,58 @@ func testReturnRecipe(id int) {
     data.Set("ID", strconv.Itoa(id))
 
     r, _ := http.Post("http://localhost:8081/return", "application/x-www-form-urlencoded; charset=utf-8", bytes.NewBufferString(data.Encode())) 
+    io.Copy(os.Stdout, r.Body)
+}
+
+func testDeleteRecipe(id int) {
+    data := url.Values{}
+    data.Set("ID", strconv.Itoa(id))
+
+    r, _ := http.Post("http://localhost:8081/delete", "application/x-www-form-urlencoded; charset=utf-8", bytes.NewBufferString(data.Encode())) 
+    io.Copy(os.Stdout, r.Body)
+}
+
+func testAlterRecipe(id int) {
+    //will double the amount of eggs in recipe with id
+    data := url.Values{}
+    data.Set("ID", strconv.Itoa(id))
+
+    //get the old version of the recipe
+    r, _ := http.Post("http://localhost:8081/return", "application/x-www-form-urlencoded; charset=utf-8", bytes.NewBufferString(data.Encode())) 
 
     decoder := json.NewDecoder(r.Body)
-    var newRecipe Recipe
-    err := decoder.Decode(&newRecipe)
-
+    var oldRecipe Recipe
+    err := decoder.Decode(&oldRecipe)
     if err != nil {
-        //panic(err)
+        panic(err)
     }
-    fmt.Println(newRecipe)
+    oldRecipe.Ingredients[0].Quantity = 2.0
 
+    //send in the alteration request
+    b := new(bytes.Buffer)
+    json.NewEncoder(b).Encode(oldRecipe)
+    res, _ := http.Post("http://localhost:8081/alter", "application/json; charset=utf-8", b)
+    io.Copy(os.Stdout, res.Body)
 }
 
 func main() {
+    for i := 0; i < 20; i++ {
+        testAddRecipe()
+    }
+    //testing delete
+    testReturnRecipe(8)
+    testDeleteRecipe(8)
+    testReturnRecipe(8)
+
+    //re-deletion?
+    testDeleteRecipe(8)
+    
+    //the delete will have shifted the indices in the "DB", check to see if return still works
+    testReturnRecipe(20)
+
+    //should return 22
     testAddRecipe()
-    testReturnRecipe(0)
-    testReturnRecipe(1)
+
+    testAlterRecipe(22)
+    
 }
